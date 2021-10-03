c6f92c97453ef5ab865a7c3ea6beb8e877e44e52
HuangOu <HuangOu@my.com>
2021-10-03 11:46:18 +0800

    First Commit

---
+++ src/main/java/pers/ho/git/GitTest1.java
@@ -0,0 +1,11 @@
+package pers.ho.git;
+
+/**
+ * @author 黄欧
+ * @create 2021-10-03 11:41
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("Hello。Git！！！");
+    }
+}
