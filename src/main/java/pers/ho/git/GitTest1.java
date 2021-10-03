b1ed41619313f444b93ac5a8654f39a17abd1360
HuangOu <HuangOu@my.com>
2021-10-03 11:49:15 +0800

    Third Commit

--- src/main/java/pers/ho/git/GitTest1.java
+++ src/main/java/pers/ho/git/GitTest1.java
@@ -7,6 +7,7 @@ package pers.ho.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Hi。Git！！！");
+        System.out.println("Hello。Git！！！");
         System.out.println("Hello。Git2！！！");
     }
 }
