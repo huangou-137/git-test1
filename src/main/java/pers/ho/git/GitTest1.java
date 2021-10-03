e799a0ce25eff096949ec603697a6ecb8cf6ccb0
HuangOu <HuangOu@my.com>
2021-10-03 11:48:26 +0800

    Second Commit

--- src/main/java/pers/ho/git/GitTest1.java
+++ src/main/java/pers/ho/git/GitTest1.java
@@ -6,6 +6,7 @@ package pers.ho.git;
  */
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("Hello。Git！！！");
+        System.out.println("Hi。Git！！！");
+        System.out.println("Hello。Git2！！！");
     }
 }
